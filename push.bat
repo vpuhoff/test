ae7542530b8f897be35548e98db1fe0831bcbb68
vpuhoff <vpuhoff@live.ru>
2019-06-01 23:14:44 +0300

    syncapi

--- push.bat
+++ push.bat
@@ -3,4 +3,4 @@ git add test.txt
 git commit -m 'test'
 git push
 git gc
-curl -vX POST http://localhost:8000/api/backup/ -d @apireq.json --header "Content-Type: application/json"
+curl -vX POST http://localhost:8000/api/syncapi -d @apireq.json --header "Content-Type: application/json"
