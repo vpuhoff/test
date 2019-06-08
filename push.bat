7de323f3ddb4b87979c1bc4376eaf2e122ff89bb
vpuhoff <vpuhoff@live.ru>
2019-06-08 21:30:18 +0300

    new scripts

--- push.bat
+++ push.bat
@@ -3,5 +3,3 @@ git add test.txt
 git commit -m 'test'
 git push
 git gc
-curl -X POST http://localhost:8000/api/syncapi?mode=sync -d @apireq.json --header "Content-Type: application/json"
-pause
---
+++ sync.bat
@@ -0,0 +1,2 @@
+curl -X POST http://localhost:8000/api/syncapi?mode=sync -d @apireq.json --header "Content-Type: application/json"
+pause
